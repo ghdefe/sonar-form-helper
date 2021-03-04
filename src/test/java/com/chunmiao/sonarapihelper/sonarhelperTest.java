064f84ec9d48c4d382433aa8b3b76cba27712667
曾春苗 <ggdefe@gmail.com>
2021-03-04 10:35:40 +0800

    [+] jenkins-api使用demo

--- src/test/java/com/chunmiao/sonarapihelper/sonarhelperTest.java
+++ src/test/java/com/chunmiao/sonarapihelper/sonarhelperTest.java
@@ -1,5 +1,6 @@
 package com.chunmiao.sonarapihelper;
 
+import com.chunmiao.sonarapihelper.service.JenkinsJobService;
 import com.chunmiao.sonarapihelper.service.SonarApiService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,14 +12,16 @@ public class sonarhelperTest {
     @Autowired
     private SonarApiService sonarApiService;
 
+    @Autowired
+    private JenkinsJobService jenkinsJobService;
 
     @Test
-    public void test(){
-//        final String allProject = sonarApiService.getAllProjectIssues();
-        sonarApiService.getProjectIssues();
-        sonarApiService.getAllProjectIssues();
-        sonarApiService.getProjectIssues();
-        sonarApiService.getAllCompanyProjectIssues();
+    public void testSonar(){
+    }
+
+    @Test
+    public void testJenkins(){
+        jenkinsJobService.login();
     }
 
 }
